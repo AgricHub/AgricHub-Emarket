6bea0390f0efe454581f43605e054f587f368c12
Daniel Ntege <danientege785@gmail.com>
2023-11-23 00:40:26 +0300

    Update index.tsx

--- website/components/Testimonials/index.tsx
+++ website/components/Testimonials/index.tsx
@@ -6,7 +6,7 @@ const testimonialData: Testimonial[] = [
   {
     id: 1,
     name: "Daniel Marvin",
-    designation: "Founder @GricHub",
+    designation: "Founder @AgricHub",
     content:
       "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
     image: "/images/testimonials/auth-01.png",
