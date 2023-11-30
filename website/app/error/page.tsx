e53f91e8b31fdb2980aaecdc8e6cc52e1fd1a5be
Kwagala Rhonitah <95081767+Rhonitah@users.noreply.github.com>
2023-11-30 08:14:47 +0300

    Update page.tsx

--- website/app/error/page.tsx
+++ website/app/error/page.tsx
@@ -144,7 +144,7 @@ const ErrorPage = () => {
                   href="/"
                   className="rounded-md bg-primary py-3 px-8 text-base font-bold text-white shadow-signUp duration-300 hover:bg-white hover:text-primary md:px-9 lg:px-8 xl:px-9"
                 >
-                  Back to Homepage
+                  Back to AgricHub Homepage
                 </Link>
               </div>
             </div>
