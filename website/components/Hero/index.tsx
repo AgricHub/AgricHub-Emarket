1f9fb69d0e1ba2940443c3b80d6bce4b3adbf1cc
Daniel Ntege <danientege785@gmail.com>
2024-04-30 09:17:08 +0300

    commit

--- website/components/Hero/index.tsx
+++ website/components/Hero/index.tsx
@@ -18,9 +18,9 @@ const Hero = () => {
                 Agricultural Marketting System.
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                  AgricHub website serves as the central hub for revolutionizing agriculture in Uganda and across Africa. Seamlessly connecting farmers, agribusinesses, retailers, and consumers, 
+                  ClyCites website serves as the central hub for revolutionizing agriculture in Uganda and across Africa. Seamlessly connecting farmers, agribusinesses, retailers, and consumers, 
                   our platform offers a dynamic online marketplace designed to simplify the buying and selling of agricultural products. 
-                  With comprehensive product listings, secure transactions, and invaluable market insights, AgricHub empowers stakeholders of all sizes to thrive in the digital age of agriculture. Join us in 
+                  With comprehensive product listings, secure transactions, and invaluable market insights, ClyCites empowers stakeholders of all sizes to thrive in the digital age of agriculture. Join us in 
                   transforming the agricultural landscape and building a sustainable future for farmers and consumers alike
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
@@ -28,7 +28,7 @@ const Hero = () => {
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                   🔥 Get Pro
+                   
                   </Link>
                   <Link
                     href="https://github.com/NextJSTemplates/startup-nextjs"
